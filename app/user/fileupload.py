777b15774a57cd4552f73ed1dd2913d4c7e9699b
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2020-10-17 23:46:28 +0800

    添加初始接口需求

--- app/user/fileupload.py
+++ app/user/fileupload.py
@@ -1,3 +1,18 @@
 from app.model import *
 from app.utils import *
 from app.user import *
+
+
+@user.route('/file/add', methods=['POST'])
+def userFileAdd():
+    pass
+
+
+@user.route('/file/delete', methods=['POST'])
+def userFileDelete():
+    pass
+
+
+@user.route('/file/view', methods=['POST'])
+def userFileView():
+    pass
--- app/user/login.py
+++ app/user/login.py
@@ -1,3 +1,18 @@
 from app.model import *
 from app.utils import *
 from app.user import *
+
+
+@user.route('/register', methods=['POST'])
+def userRegister():
+    pass
+
+
+@user.route('/login', methods=['POST'])
+def userLogin():
+    pass
+
+
+@user.route('/logout', methods=['POST'])
+def userLogout():
+    pass
